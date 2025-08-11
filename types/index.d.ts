88808a2bdd85b3d0ce8fc83a4c178ad1a9aa5e55
Andreeff Alexander <alexanderfromearth@gmail.com>
2025-08-11 08:20:53 +0500

    fix: fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,19 @@
 import {type LogFn} from 'pino';
 import {type RedisClientPoolType} from 'redis';
 
-export interface Modules {
+export default function service(): Promise<void>;
+
+export type Service<T, U> = (
+  params: T,
+  modules: ModuleActions,
+  meta: {links: Record<string, string>}
+) => U | Promise<U>;
+
+export type ModuleActions = {
+  [ModuleName in keyof Modules]: ReturnType<Modules[ModuleName]>['action'];
+};
+
+export interface Modules extends Record<string, ModuleFactory<any, any>> {
   sql: ModuleFactory<{
     query<T>(query: string, ...args: any[]): Promise<T>;
     raw(query: string, ...args: any[]): any;
@@ -21,6 +33,7 @@ export type SystemModuleActions = {
 
 interface Logger {
   child(name?: string): Logger;
+
   fatal: LogFn;
   error: LogFn;
   warn: LogFn;
@@ -33,5 +46,6 @@ export type ModuleFactory<T, Args extends Array<any>> = () => Module<T, Args>
 
 export interface Module<T, Args extends Array<any>> {
   action(...args: Args): T;
+
   dispose?(): Promise<void>;
 }
